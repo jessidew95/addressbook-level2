95791dc5022165b0f09e2d244401d55087fb4aa3
jessidew95 <dewi-jessi-jiahui@hotmail.com>
2016-08-31 00:18:28 +0800

    Create Unit Class

---
+++ src/seedu/addressbook/data/person/Unit.java
@@ -0,0 +1,61 @@
+package seedu.addressbook.data.person;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+
+/**
+ * Represents a Person's unit in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidUnit(String)}
+ */
+public class Unit {
+
+    public static final String EXAMPLE = "#12-345";
+    public static final String MESSAGE_UNIT_CONSTRAINTS = 
+    		"Person unit should be a '#' followed by 2 numeric strings separated by '-'";
+    public static final String UNIT_VALIDATION_REGEX = "#\\d+-\\d+";
+
+    public final String value;
+    private boolean isPrivate;
+
+    /**
+     * Validates given unit.
+     *
+     * @throws IllegalValueException if given unit string is invalid.
+     */
+    public Unit(String unit, boolean isPrivate) throws IllegalValueException {
+        this.isPrivate = isPrivate;
+        unit = unit.trim();
+        if (!isValidUnit(unit)) {
+            throw new IllegalValueException(MESSAGE_UNIT_CONSTRAINTS);
+        }
+        this.value = unit;
+    }
+
+    /**
+     * Checks if a given string is a valid person unit number.
+     */
+    public static boolean isValidUnit(String test) {
+        return test.matches(UNIT_VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Unit // instanceof handles nulls
+                && this.value.equals(((Unit) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+}
