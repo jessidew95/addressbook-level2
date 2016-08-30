481062334f8c19128adae31f250ada5c065784e8
jessidew95 <dewi-jessi-jiahui@hotmail.com>
2016-08-30 23:53:21 +0800

    Create Block Class

---
+++ src/seedu/addressbook/data/person/Block.java
@@ -0,0 +1,57 @@
+package seedu.addressbook.data.person;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+
+public class Block {
+	
+	public static final String EXAMPLE = "123";
+    public static final String MESSAGE_BLOCK_CONSTRAINTS = "Person block number should only contain numbers";
+    public static final String BLOCK_VALIDATION_REGEX = "\\d+";
+	
+	private final String value;
+	private boolean isPrivate;
+	
+    /**
+     * Validates given phone number.
+     *
+     * @throws IllegalValueException if given phone string is invalid.
+     */
+    public Block(String block, boolean isPrivate) throws IllegalValueException {
+        this.isPrivate = isPrivate;
+        block = block.trim();
+        if (!isValidPhone(block)) {
+            throw new IllegalValueException(MESSAGE_BLOCK_CONSTRAINTS);
+        }
+        this.value = block;
+    }
+
+    /**
+     * Checks if a given string is a valid person phone number.
+     */
+    public static boolean isValidPhone(String test) {
+        return test.matches(BLOCK_VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Block // instanceof handles nulls
+                && this.value.equals(((Block) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+	
+	
+}
