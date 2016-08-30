c18e632006c757d3f5b86e5154b2e00f0b0c1d59
jessidew95 <dewi-jessi-jiahui@hotmail.com>
2016-08-31 00:38:53 +0800

    Change Street Regex

--- src/seedu/addressbook/data/person/Street.java
+++ src/seedu/addressbook/data/person/Street.java
@@ -10,7 +10,7 @@ public class Street {
 
     public static final String EXAMPLE = "Orange Avenue 2";
     public static final String MESSAGE_STREET_CONSTRAINTS = "Person street should be alphanumeric";
-    public static final String STREET_VALIDATION_REGEX = "[\\w\\.]+";
+    public static final String STREET_VALIDATION_REGEX = "\\w+";
 
     public final String value;
     private boolean isPrivate;
